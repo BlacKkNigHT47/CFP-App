81e230ae282e25a70110582179135e01b5682d12
WhiteGravel <62961736+WhiteGravel@users.noreply.github.com>
2022-02-19 20:48:33 +0000

    Added 3 more items

--- src/plugins/lookup-table.js
+++ src/plugins/lookup-table.js
@@ -1,70 +1,95 @@
 var lookUpTable = {
-    "8003340091471": {
-                        "name": "Lindt Sprungli chocolate",
-                        "country": "Switzerland",
-                        "price": 6.00,
-                        "CFPPrimary": 2.00,
-                        "CFPGlobal": 10.00,
-                        "CFPLocal":2.00,
-                        "VAT":20.00
-                     },
-    "8906021122108": {
-         
-                        "name": "Achi's Cut Mango Pickle",
-                        "country": "India",
-                        "price": 2.49,
-                        "CFPPrimary": 0.00,
-                        "CFPGlobal": 20.00,
-                        "CFPLocal":2.00,
-                        "VAT":20.00
-                     },
-        "01771367": {
-         
-                        "name": "STRAWBERRIES by Sainsburys",
-                        "country": "Spain",
-                        "price": 2.00,
-                        "CFPPrimary": 0.00,
-                        "CFPGlobal": 20.00,
-                        "CFPLocal":2.00,
-                        "VAT":20.00
-                     },
-       "5012008561402" : {
-                     "name": "Cuticura Original Hand Sanitiser",
-                     "country": "UK",
-                     "price": 2.00,
-                     "CFPPrimary": 0,
-                     "CFPGlobal": 0,
-                     "CFPLocal": 2.00,
-                     "VAT":20.0
-       },
-       "5060201181316" : {
-         "name": "Pink Panther Wafers",
-         "country": "UK",
-         "price": 2.00,
-         "CFPPrimary": 0,
-         "CFPGlobal": 0,
-         "CFPLocal": 2.00,
-         "VAT":20.0
-    },
-    "3574660541298" : {
+   "8003340091471": {
+      "name": "Lindt Sprungli chocolate",
+      "country": "Switzerland",
+      "price": 6.00,
+      "CFPPrimary": 2.00,
+      "CFPGlobal": 10.00,
+      "CFPLocal": 2.00,
+      "VAT": 20.00
+   },
+   "8906021122108": {
+
+      "name": "Achi's Cut Mango Pickle",
+      "country": "India",
+      "price": 2.49,
+      "CFPPrimary": 0.00,
+      "CFPGlobal": 20.00,
+      "CFPLocal": 2.00,
+      "VAT": 20.00
+   },
+   "01771367": {
+
+      "name": "STRAWBERRIES by Sainsburys",
+      "country": "Spain",
+      "price": 2.00,
+      "CFPPrimary": 0.00,
+      "CFPGlobal": 20.00,
+      "CFPLocal": 2.00,
+      "VAT": 20.00
+   },
+   "5012008561402": {
+      "name": "Cuticura Original Hand Sanitiser",
+      "country": "UK",
+      "price": 2.00,
+      "CFPPrimary": 0,
+      "CFPGlobal": 0,
+      "CFPLocal": 2.00,
+      "VAT": 20.0
+   },
+   "5060201181316": {
+      "name": "Pink Panther Wafers",
+      "country": "UK",
+      "price": 2.00,
+      "CFPPrimary": 0,
+      "CFPGlobal": 0,
+      "CFPLocal": 2.00,
+      "VAT": 20.0
+   },
+   "3574660541298": {
       "name": "Aveeno Daily Moist.",
       "country": "UK",
       "price": 2.00,
       "CFPPrimary": 0,
       "CFPGlobal": 0,
       "CFPLocal": 2.00,
-      "VAT":20.0
+      "VAT": 20.0
    },
-   "5011417563687" : {
+   "5011417563687": {
       "name": "Dettol Anti Bact.",
       "country": "UK",
       "price": 7.00,
       "CFPPrimary": 0,
       "CFPGlobal": 0,
       "CFPLocal": 2.00,
-      "VAT":20.0
+      "VAT": 20.0
    },
-       
-
-   };
+   "3045320505535": {
+      "name": "Bon Maman Raspberry Conserve",
+      "country": "France",
+      "price": 2.99,
+      "CFPPrimary": 5.00,
+      "CFPGlobal": 5.00,
+      "CFPLocal":2.00,
+      "VAT":20.00
+   },
+"9332934295889": {
+      "name": "Smiggle Letter Keyring (Letter K)",
+      "country": "China",
+      "price": 1.99,
+      "CFPPrimary": 5.00,
+      "CFPGlobal": 5.00,
+      "CFPLocal":2.00,
+      "VAT":0.50
+},
+"8714789710020": {
+   "name": "Colgate Antibacterial & Fluoride Toothpaste",
+   "country": "United Kingdom",
+   "price": 6.99,
+   "CFPPrimary": 2.50,
+   "CFPGlobal": 5.00,
+   "CFPLocal":2.00,
+   "VAT":2.00
+},
+};
 export default lookUpTable;
